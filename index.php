fc9537d650f6962fd1d47c6ed678579204d6f742
Paolo <p.fiaschi@hotmail.it>
2020-05-07 10:31:35 +0200

    utilizzo del require/include

--- index.php
+++ index.php
@@ -5,37 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>php snack</title>
-    <?php
-
-
+    <?php 
+        require_once "db.php";
     ?>
 </head>
 <body>
-    <!-- 10 numeri casuali, poi il min, max e media-->
+    <!-- stampare nome descrizione e voto di ogni hotel -->
     <h1>
         <?php 
-            
-            $min = 100;
-            $max = 1;
-            $avg = 0;
-            for ($i=0; $i < 10; $i++) { 
-                $myNewInt = rand(1, 100);
-                if ($myNewInt < $min) {
-                   $min = $myNewInt;
-                }
-                if ($myNewInt > $max) {
-                    $max = $myNewInt;
-                }
-                $avg += $myNewInt;
-                echo $myNewInt . "<br>";
+
+            foreach ($hotels as $key => $hotel) {
+                
+                echo $hotel ["name"] . "<br>";
+                echo $hotel ["description"] . "<br>";
+                echo $hotel ["vote"] . "<br>-----------<br>";
+                
             }
-            $avg /= 10; //$avg = $avg /10
-            echo "<br>-----------------<br>";
-            echo "min: " . $min . "<br>";
-            echo "max: " . $max . "<br>";
-            echo "avg: " . $avg . "<br>";
-
-
+    
         ?>
 
     </h1>

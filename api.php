ca1c4fc52cc2837ebfc26793a888a9c0c8352e22
Paolo <p.fiaschi@hotmail.it>
2020-05-08 12:46:43 +0200

    terminato

--- api.php
+++ api.php
@@ -2,12 +2,15 @@
     header('Content-Type: application/json');   
     require_once "db.php"; 
     $res = [];
-
-
- 
+    
     foreach ($class as $student) {
         
-        $res[] = $student["name"] . " " . $student["lastname"];
+        $res[] = [
+            "name" => $student["name"] . " " . $student["lastname"],
+            "scores" => $student["scores"]
+        ];
     }
     echo json_encode($res);
+
+
 ?>
